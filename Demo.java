9dca34d9f7bd3a7cc2cbfa5d531f5e5a644e9425
abhisheksharma558 <abhiaideveloper@gmail.com>
2026-07-18 11:53:04 +0530

    Update and rename demo.java to Demo.java

---
+++ Demo.java
@@ -0,0 +1,5 @@
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("jenkins 123! hello");
+    }
+}
--- demo.java
+++
@@ -1,5 +0,0 @@
-public class Demo {
-  public static void main(String[] args) {
-    system.out.println("jenkins 123! hello");
-  }
-}
